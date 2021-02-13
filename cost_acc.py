72be939bf820f460e0dfe6fd3c64ae36883beec5
Anindyadeep <58508471+Anindyadeep@users.noreply.github.com>
2021-02-13 11:23:23 +0530

    Update cost_acc.py

--- cost_acc.py
+++ cost_acc.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def binary_loss(A, Y):
     m = A.shape[1]
-    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)))
+    cost = -1 / m * np.sum(Y * np.log(A + 1e-8) + (1 - Y) * (np.log(1 - A + 1e-8)))
     return cost
 
 
